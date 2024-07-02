ea5f2fa73b667aab709ccc72d2abf13d41d023b0
Vishal Dhayalan <vishal.dhayalan@gmail.com>
2024-07-02 23:05:58 +0100

    Stream transcript in chunks

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
-from starlette.websockets import WebSocketDisconnect
-
 
 app = FastAPI()
+
+# Maintain current prompt of users. A simpler alternative to in-memory DBs and session variables in this case.
+# This prompt is built via concatenation of STT chunks streamed from the frontend.
+user_prompt: dict[WebSocket, str] = {}
 
 # Mount the directory containing the HTML file as a static files directory
 app.mount("/static", StaticFiles(directory="static"), "static")
@@ -13,15 +15,23 @@ app.mount("/static", StaticFiles(directory="static"), "static")
 async def get():
     return HTMLResponse(open("static/index.html").read())
 
+# TODO: rename ws endpoint based on streaming voice or text
 @app.websocket("/speech-query")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
         while True:
-            transcript = await websocket.receive_text()
-            print(transcript, end="", flush=True)
+            transcript_chunk = await websocket.receive_text()
+            if transcript_chunk == "<end>":
+                # Send to LLM
+                print(f"\nSENDING TO LLM!\n{user_prompt[websocket]}", flush=True)
+            else:
+                # Append transcript to user's current prompt, creating an item for the prompt only when the first chunk is received.
+                user_prompt[websocket] = user_prompt.get(websocket, "") + transcript_chunk
+                print(transcript_chunk, end="", flush=True)
     except WebSocketDisconnect:
         print("Client disconnected")
+        del user_prompt[websocket]
 
 if __name__ == "__main__":
     import uvicorn
--- static/script.js
+++ static/script.js
@@ -4,43 +4,19 @@ const transcriptionDiv = document.getElementById('transcription');
 
 let ws = new WebSocket('wss://' + location.hostname + ':8888/speech-query');
 let recording = false;
-let transcriptCharIdx = 0
 
 recognition = new window.webkitSpeechRecognition();
 recognition.lang = 'en-US';
-recognition.interimResults = true;
+recognition.interimResults = false;
 recognition.continuous = true;
 
 recognition.onresult = function(event) {
     console.log(event.results)
-    let result = '';
-    let curCharIdx = 0;
-    let i = 0;
-
-    // Skip all results that have already been streamed (i.e. everything up to `transcriptCharIdx`).
-    while (
-        i < event.results.length
-        &&
-        curCharIdx + event.results[i][0].transcript.length < transcriptCharIdx
-    ) {
-        curCharIdx += event.results[i][0].transcript.length;
-        i += 1;
-    }
-
-    // Get only portion of results that is new (i.e. `transcriptCharIdx` onwards).
-    if (i < event.results.length) {
-        result += event.results[i][0].transcript.slice(transcriptCharIdx - curCharIdx)
-        i += 1
-        while (i < event.results.length) {
-            result += event.results[i][0].transcript
-            i += 1
-        }
-    }
 
     // Append to user transcript textbox and send to backend via websocket.
-    transcriptCharIdx += result.length
-    transcriptionDiv.textContent += result;
-    ws.send(result);
+    transcript = event.results[event.results.length - 1][0].transcript
+    transcriptionDiv.textContent += transcript
+    ws.send(transcript);
 };
 
 recognition.onerror = function(event) {
@@ -50,8 +26,7 @@ recognition.onerror = function(event) {
 };
 
 recognition.onstart = function() {
-    // STT started. Reset transcription index and add 'recording mode' styling for button.
-    transcriptCharIdx = 0;
+    // STT started. Add 'recording mode' styling for button.
     micIcon.classList.add('fa-beat-fade')
     micIcon.classList.add('recording')
 }
@@ -60,6 +35,7 @@ recognition.onend = function() {
     // STT ended. Remove 'recording mode' styling for button.
     micIcon.classList.remove('fa-beat-fade')
     micIcon.classList.remove('recording')
+    ws.send("<end>")
 }
 
 micButton.onclick = async () => {
